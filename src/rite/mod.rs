2c21e2c157e0177c7b9af062e6e234f0234f26b6
Siddharth Singh <simsid66@gmail.com>
2021-10-17 16:17:43 +0530

    add client view and delete routes

--- src/rite/mod.rs
+++ src/rite/mod.rs
@@ -6,7 +6,7 @@ pub mod routes;
 pub mod middleware;
 
 use oauth_config::OauthConfig;
-use sqlx::{SqlitePool, types::chrono::NaiveDateTime};
+use sqlx::{FromRow, Row, SqlitePool, sqlite::SqliteRow, types::chrono::NaiveDateTime};
 use tide_tera::{TideTeraExt, context};
 
 #[derive(Clone, Debug)]
@@ -29,14 +29,26 @@ pub struct UploadRequest {
 #[derive(Clone, Debug, serde::Deserialize)]
 pub struct LinkRequest {
     pub nickname: String,
-    pub code: String
+    pub token: String
 }
 
-#[derive(Clone, Debug, serde::Deserialize)]
-pub struct PendingClientRow {
-    pub uuid: String,
-    pub added_on: NaiveDateTime,
-    pub user: String
+#[derive(Clone, Debug, Default, serde::Deserialize, serde::Serialize)]
+pub struct Client {
+    pub added_on: String,
+    pub user: String,
+    pub nickname: String,
+    pub uuid: String
+}
+
+impl FromRow<'_, SqliteRow> for Client {
+    fn from_row<'r>(row: &'r SqliteRow) -> Result<Self, sqlx::Error> {
+        let mut rv = Client::default();
+        rv.added_on = row.try_get::<NaiveDateTime, &str>("added_on")?.to_string();
+        rv.user = row.try_get("user")?;
+        rv.nickname = row.try_get("nickname")?;
+        rv.uuid = row.try_get("uuid")?;
+        Ok(rv)
+    }
 }
 
 pub fn server_error(tera: tera::Tera, title: &str, msg: &str) -> tide::Result {
--- src/rite/routes.rs
+++ src/rite/routes.rs
@@ -1,9 +1,9 @@
 use crate::{
-    rite::{server_error, LinkRequest},
+    rite::{server_error, Client, LinkRequest},
     State,
 };
 use http_types::StatusCode;
-use sqlx::{types::chrono::NaiveDateTime, Execute, Executor, Row};
+use sqlx::{Row, Sqlite, types::chrono::NaiveDateTime};
 use tera::Context;
 use tide::{Redirect, Request, Response};
 use tide_tera::{context, TideTeraExt};
@@ -31,14 +31,14 @@ pub async fn link_client_get(req: Request<State>) -> tide::Result {
         context.try_insert("username", &val)?;
         username = val;
     }
-    let uuid = Uuid::new_v4();
+    let token = Uuid::new_v4();
     context.try_insert("section", "link a client")?;
-    context.try_insert("code", &uuid.to_string())?;
+    context.try_insert("token", &token.to_string())?;
     let tera = req.state().tera.clone();
     let mut db = req.state().rite_db.clone().acquire().await?;
 
-    sqlx::query("insert into pending_clients(uuid, user, added_on) values(?, ?, datetime('now'));")
-        .bind(uuid.to_string())
+    sqlx::query("insert into pending_clients(token, user, added_on) values(?, ?, datetime('now'));")
+        .bind(token.to_string())
         .bind(username)
         .execute(&mut db)
         .await?;
@@ -57,6 +57,33 @@ pub async fn link_client_get(req: Request<State>) -> tide::Result {
     }
 }
 
+pub async fn delete_client(mut req: Request<State>) -> tide::Result {
+    let state = req.state();
+    let tera = state.tera.clone();
+    let mut db;
+    if let Ok(val) = state.rite_db.acquire().await {
+        db = val;
+    }
+    else {
+        return server_error(tera, "Error acquiring database connection", "");
+    }
+    let username: String;
+    if let Some(val) = req.session().get("username") {
+        username = val;
+    }
+    else {
+        return server_error(tera, "Unknown error", "Username was None trying to read session");
+    }
+
+    sqlx::query("delete from clients where user = ? and uuid = ?;")
+    .bind(username)
+    .bind(&req.param("uuid")?)
+    .execute(&mut db)
+    .await?;
+
+    Ok(Redirect::new("/clients/view").into())
+}
+
 pub async fn link_client_post(mut req: Request<State>) -> tide::Result {
     let session = req.session();
     let state = req.state();
@@ -72,19 +99,19 @@ pub async fn link_client_post(mut req: Request<State>) -> tide::Result {
     let link_req: LinkRequest = req.body_form().await?;
 
     let mut conn = db.acquire().await?;
-    let result= sqlx::query("select * from pending_clients where uuid is ? and user is ? and Cast((JulianDay(datetime('now')) - JulianDay(added_on)) * 24 * 60 * 60 As Integer) < ?;")
-        .bind(link_req.code)
+    let result= sqlx::query("select * from pending_clients where token is ? and user is ? and Cast((JulianDay(datetime('now')) - JulianDay(added_on)) * 24 * 60 * 60 As Integer) < ?;")
+        .bind(link_req.token)
         .bind(username)
         .bind(300).fetch_optional(&mut conn).await?;
 
     let dt: NaiveDateTime;
-    let uuid: String;
+    let token: String;
     let user: String;
     if let Some(val) = result {
         dt = val.try_get("added_on")?;
-        uuid = val.try_get("uuid")?;
+        token = val.try_get("token")?;
         user = val.try_get("user")?;
-        println!("{}, {}, {}", dt, uuid, user);
+        println!("{}, {}, {}", dt, token, user);
     } else {
         return server_error(
             tera,
@@ -93,29 +120,49 @@ pub async fn link_client_post(mut req: Request<State>) -> tide::Result {
         );
     }
 
-    sqlx::query("delete from pending_clients where uuid is ? and user is ?")
-        .bind(&uuid)
+    sqlx::query("delete from pending_clients where token is ? and user is ?")
+        .bind(&token)
         .bind(&user)
         .execute(&mut conn)
         .await?;
 
-    sqlx::query("insert into clients(uuid, user, nickname, added_on) values(?, ?, ?, datetime('now'));")
-        .bind(uuid)
-        .bind(user)
-        .bind(link_req.nickname)
-        .execute(&mut conn)
-        .await?;
+    let uuid = Uuid::new_v4().to_string();
+    sqlx::query(
+        "insert into clients(uuid, token, user, nickname, added_on) values(?, ?, ?, ?, datetime('now'));",
+    )
+    .bind(uuid)
+    .bind(token)
+    .bind(user)
+    .bind(link_req.nickname)
+    .execute(&mut conn)
+    .await?;
 
     Ok(Redirect::new("/clients/view").into())
 }
 
 pub async fn view_clients(req: Request<State>) -> tide::Result {
     let session = req.session();
-    let mut context = Context::new();
-    if let Some(username) = session.get::<String>("username") {
-        context.try_insert("username", &username)?;
+    let tera = req.state().tera.clone();
+    let mut context = context!{
+        "section" => "view clients"
+    };
+    let username: String;
+    if let Some(val) = session.get::<String>("username") {
+        username = val.clone();
+        context.try_insert("username", &val)?;
     }
-    unimplemented!();
+    else {
+        return server_error(tera, "Unknown error", "Username was None trying to read session");
+    }
+    let mut db = req.state().rite_db.clone().acquire().await?;
+
+    let rows: Vec<Client> = sqlx::query_as::<Sqlite, Client>("SELECT uuid, user, nickname, added_on from clients where user = ?;")
+        .bind(username)
+        .fetch_all(&mut db).await?;
+
+    context.try_insert("rows", &rows)?;
+
+    tera.render_response("view_clients.html", &context)
 }
 
 pub async fn doc_upload(mut req: Request<State>) -> tide::Result {
